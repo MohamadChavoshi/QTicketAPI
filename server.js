5883c6e8ce9f60e5d2fe91f0bad1a2a70f968148
Mohammad Reza Chavoshi <115008720+MohamadChavoshi@users.noreply.github.com>
2022-10-30 15:07:06 -0700

    Mysql2 has been added to project

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const db = require('mysql2')
 const app = express()
 
 
 app.get('/', (req,res) => {
-    res.sendStatus(200).send({msg : "Connected Successful"})
-    // test new pull request
+    res.status(200).send({
+         Status : "Connecttion Successful"
+})
+})
+
+app.post('/register' , (req,res) =>{
+    const { username , password } = req.query
+    const { authorization } = req.header
+    if(authorization && authorization == "test"){
+        
+    }
 })
 
 
