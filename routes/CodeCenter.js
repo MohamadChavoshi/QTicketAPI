341bcfa7b99482c1640e2415739ff81ace60f19e
Mohammad Reza Chavoshi <115008720+MohamadChavoshi@users.noreply.github.com>
2022-12-05 11:38:10 -0800

    codeValidation & refactor storingcode

--- routes/CodeCenter.js
+++ routes/CodeCenter.js
@@ -1,18 +1,46 @@
 const { Router } = require('express');
 const router = Router();
+const {TokenValidator} = require('../lib/Validation')
 knex = require('../lib/database');
+
 
 
 
 
 router.post('/storingcode', async (req,res) => {
     var { RECCode , GUID} = req.query
-    if(accessToken(req.headers.authorization) && RECCode && GUID){
-        knex('coderepo').insert({Code: RECCode, GUID: GUID , accesstoken: req.headers.authorization})
+    if(req.headers.authorization && RECCode && GUID){
+        if(TokenValidator(req.headers.authorization)){
+            knex('coderepo').insert({Code: RECCode, GUID: GUID , accesstoken: req.headers.authorization})
+        }else{
+            res.status(403).send("Permission denied : 403 ERROR unathorized")
+        }
     }else {
-        res.status(403).send("403 Error unauthorized")
+        res.status(400).send("400 Error BAD USER INPUT")
     }
 
+})
+
+
+
+router.post('/codevalidator', async (req,res) => {
+    var { RECCode, GUID} = req.query
+    if(RECCode && req.headers.authorization && GUID){
+        if(TokenValidator(req.headers.authorization)){
+            let resault = await knex('coderepo').select('Code').where('GUID',GUID)
+            resault = resault[0]
+            if(resault == undefined){ 
+                res.status(404).send({ res : 0})
+            }
+            else{
+                res.status(200).send({ res : 1})
+            }
+        }else{
+            res.status(403).send("Permission denied : 403 ERROR unathorized")
+        }
+    }else{
+        res.status(400).send({ msg : "ERROR 400 : BAD USER INPURT"})
+    }
 })
 
 
